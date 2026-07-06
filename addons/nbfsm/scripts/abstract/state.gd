e6420938d3f8392e3929ecf155e0c054030aa0f7
Ivan <ivanbabaev217@gmail.com>
2026-07-06 21:32:21 +0800

    Stop ignoring warnings

--- addons/nbfsm/scripts/abstract/state.gd
+++ addons/nbfsm/scripts/abstract/state.gd
@@ -9,17 +9,14 @@ func enter() -> void:
 	pass
 
 
-@warning_ignore("unused_parameter")
 func input_update(event: InputEvent) -> void:
 	pass
 
 
-@warning_ignore("unused_parameter")
 func update(delta: float) -> void:
 	pass
 
 
-@warning_ignore("unused_parameter")
 func physics_update(delta: float) -> void:
 	pass
 
